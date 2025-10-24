304685c5d1370777eb6bdfcdbc3fac671e8ab9d4
CodeNedoC <jatinmangtani@gmail.com>
2025-10-24 05:08:45 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -43,25 +43,14 @@ app.use(cors({
 // Update cors configuration
 const io = new Server(httpServer, {
   cors: {
-    origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.CLIENT_URL
-        : allowedOrigins,
+    origin: allowedOrigins, 
     credentials: true,
     methods: ["GET", "POST"],
   },
   transports: ["websocket", "polling"],
 });
 
-if (process.env.NODE_ENV === "production") {
-  // Serve static files from the frontend build
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-  });
-}
 
 app.use(express.json());
 
