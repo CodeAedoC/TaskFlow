f29a24812728b64d36fb1d2fa61c6b37a1ef4e70
Jatin Mangtani Priyani <jatinmangtani@gmail.com>
2025-10-24 15:40:23 +0100

    Update user.models.js

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
 
     emailVerified: {
       type: Boolean,
-      default: true,
+      default: false,
     },
     emailVerificationToken: String,
     emailVerificationExpires: Date,
